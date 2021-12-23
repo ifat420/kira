fefef7a47ed4cb92b55b60da69a1206f6a9f1416
iftakhar ahmed <ifat802@gmail.com>
2021-12-23 12:22:27 +0600

    ✨ colleciton page research

---
+++ assets/global.js
@@ -0,0 +1,26 @@
+function debounce(fn, wait) {
+    let t;
+    return (...args) => {
+        clearTimeout(t);
+        t = setTimeout(() => fn.apply(this, args), wait);
+    };
+}
+
+function fetchConfig(type = 'json') {
+    return {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Accept': `application/${type}` }
+    };
+}
+
+function onKeyUpEscape(event) {
+    if (event.code.toUpperCase() !== 'ESCAPE') return;
+
+    const openDetailsElement = event.target.closest('details[open]');
+    if (!openDetailsElement) return;
+
+    const summaryElement = openDetailsElement.querySelector('summary');
+    openDetailsElement.removeAttribute('open');
+    summaryElement.setAttribute('aria-expanded', false);
+    summaryElement.focus();
+}
